cf3915ba6a1306466ed62558566784b08096dbb1
im-hary <hariharan934526@gmail.com>
2024-07-17 11:49:29 +0530

    then commit

--- client/crud/src/Createusers.jsx
+++ client/crud/src/Createusers.jsx
@@ -10,7 +10,7 @@ function Createusers() {
 
     const Submit = (e) => {
         e.preventDefault();
-        axios.post("https://crud-server-seven-taupe.vercel.app/createUser", { name, email, age })
+        axios.post("http://localhost:3001/createUser", { name, email, age })
             .then(result => {
                 console.log(result)
                 navigate('/')
